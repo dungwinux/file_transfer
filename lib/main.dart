e63ef7eb1c1e3eb13100eb067d90a62053c1467e
Dung Tuan Nguyen <ntddebugger@gmail.com>
2020-12-20 02:11:10 +0700

    fix: removing dead code

--- lib/main.dart
+++ lib/main.dart
@@ -4,57 +4,6 @@ void main() {
   runApp(MyApp());
 }
 
-class MyTabbedPage extends StatefulWidget {
-  const MyTabbedPage({ Key key }) : super(key: key);
-  @override
-  _MyTabbedPageState createState() => _MyTabbedPageState();
-}
-
-class _MyTabbedPageState extends State<MyTabbedPage> with SingleTickerProviderStateMixin {
-  final List<Tab> myTabs = <Tab>[
-    Tab(text: 'LEFT'),
-    Tab(text: 'RIGHT'),
-  ];
-
-  TabController _tabController;
-
-  @override
-  void initState() {
-    super.initState();
-    _tabController = TabController(vsync: this, length: myTabs.length);
-  }
-
- @override
- void dispose() {
-   _tabController.dispose();
-   super.dispose();
- }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        bottom: TabBar(
-          controller: _tabController,
-          tabs: myTabs,
-        ),
-      ),
-      body: TabBarView(
-        controller: _tabController,
-        children: myTabs.map((Tab tab) {
-          final String label = tab.text.toLowerCase();
-          return Center(
-            child: Text(
-              'This is the $label tab',
-              style: const TextStyle(fontSize: 36),
-            ),
-          );
-        }).toList(),
-      ),
-    );
-  }
-}
-
 class MyApp extends StatelessWidget {
   MyApp({Key key}) : super(key: key);
   // This widget is the root of your application.
@@ -116,81 +65,59 @@ class _MyHomePageState extends State<MyHomePage> {
   Widget build(BuildContext context) {
     return DefaultTabController(
       length: 2,
-      child: Builder(
-        builder: (BuildContext context) {
+      child: Builder(builder: (BuildContext context) {
         final TabController tabController = DefaultTabController.of(context);
         tabController.addListener(() {
           if (tabController.indexIsChanging) {
-            setState(() {
-            if(tabController.index == 0)
-            {
-              floatingActionButton: FloatingActionButton(
-              onPressed: _incrementCounter,
-              tooltip: 'Add',
-              child: Icon(Icons.add),
-              );
-            }
-            else
-            {
-              floatingActionButton: FloatingActionButton(
-              onPressed: _incrementCounter,
-              tooltip: 'Download',
-              child: Icon(Icons.download_rounded),
-              ); 
-            }
-            });
+            // Bad practice
+            // TODO: actually changing a certain state
+            setState(() {});
           }
         });
         return Scaffold(
-        appBar: AppBar(
-          title: Text(widget.title),
-          centerTitle: true,
-          bottom: TabBar(tabs: [
-            Tab(
-                child: Text(
-              'Send',
-            )),
-            Tab(
-                child: Text(
-              'Receive',
-            ))
-          ]),
-        ),
-        body: TabBarView(children: [
-          ListView.builder(
-              itemCount: items.length,
-              itemBuilder: (context, index) {
-                return ListTile(
-                  title: Text('${items[index]}'),
-                );
-              }),
-          ListView.builder(
-              itemCount: items.length,
-              itemBuilder: (context, index) {
-                return ListTile(
-                  title: Text('${items[index]}'),
-                );
-              }),
-        ]),
-        floatingActionButton: Builder(builder: (context) {
-            final index = DefaultTabController.of(context).index;
-            if (index == 0)
-              return FloatingActionButton(
-                onPressed: _incrementCounter,
-                tooltip: 'Add',
-                child: Icon(Icons.add),
-              );
-            else
-              return FloatingActionButton(
-                onPressed: _incrementCounter,
-                tooltip: 'Download',
-                child: Icon(Icons.download_rounded),
-              );
-          }),
-      );
-      }
-      ),
+          appBar: AppBar(
+            title: Text(widget.title),
+            centerTitle: true,
+            bottom: TabBar(
+              tabs: [
+                Tab(child: Text('Send')),
+                Tab(child: Text('Receive')),
+              ],
+            ),
+          ),
+          body: TabBarView(
+            children: [
+              ListView.builder(
+                itemCount: items.length,
+                itemBuilder: (context, index) {
+                  return ListTile(
+                    title: Text('${items[index]}'),
+                  );
+                },
+              ),
+              ListView.builder(
+                itemCount: items.length,
+                itemBuilder: (context, index) {
+                  return ListTile(
+                    title: Text('${items[index]}'),
+                  );
+                },
+              ),
+            ],
+          ),
+          floatingActionButton: (DefaultTabController.of(context).index == 0)
+              ? FloatingActionButton(
+                  onPressed: _incrementCounter,
+                  tooltip: 'Add',
+                  child: Icon(Icons.add),
+                )
+              : FloatingActionButton(
+                  onPressed: _incrementCounter,
+                  tooltip: 'Download',
+                  child: Icon(Icons.download_rounded),
+                ),
+        );
+      }),
     );
   }
 }
-
